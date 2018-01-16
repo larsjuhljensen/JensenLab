6c7c3c7bb52fbea0d073c58d5e9141d9a1d8e03a
larsjuhljensen <lars.juhl.jensen@gmail.com>
2018-01-16 06:42:35 +0100

    Changed network node colors to match CPR logo.

--- assets/particles.js
+++ assets/particles.js
@@ -86,7 +86,8 @@
 			density: 15000, // the lower the denser
 			netLineDistance: 200,
 			netLineColor: '#99cccc',
-			particleColors: ['#6D4E5C', '#aaa', '#FFC458' ]
+			particleColors: ['#B9282E', '#B9187B', '#439750' ]
+			//particleColors: ['#6D4E5C', '#aaa', '#FFC458' ]
 			//particleColors: ['#aaa'] // ['#6D4E5C', '#aaa', '#FFC458' ]
 		};
 		this.canvas = parent.canvas;
